a22352386f85f7fde02ecbd8a9970baae60c52e5
lucidixp <alucidi90@gmail.com>
2017-01-09 14:03:02 -0500

    Update sketch.js

--- fractal_tree_mobile/sketch.js
+++ fractal_tree_mobile/sketch.js
@@ -11,7 +11,7 @@ function setup() {
   
 }
 
-   function touchMoved() {
+   function touchStarted() {
    for(var i = tree.length -1; i >= 0; i--) {
      if (!tree[i].finished) {
       tree.push(tree[i].branchA());
